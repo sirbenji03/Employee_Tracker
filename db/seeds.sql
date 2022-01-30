e38d59929b84f62c3d54e9facac7745ce9df5ada
benjamin amakye <bamakye20@gmail.com>
2022-01-29 23:57:31 -0500

    another commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,8 +9,17 @@ VALUES
 INSERT INTO job (title, salary, department_id)
 VALUES 
 ('Director', 75000, 1),
-('Associate', 18000, 1),
-('Head Auditor', 100000, 2),
-('Accountant', 80000, 2),
-('Engineer', 55000, 3),
-('Sales rep', 0, 1);
+('Associate', 18000, 2),
+('Head Auditor', 100000, 3),
+('Accountant', 80000, 4),
+('Engineer', 55000, 5),
+('Sales rep', 0, 6);
+
+INSERT INTO employee (first_name, last_name, job_id, manager_id)
+VALUES 
+('Ronaldo', 'Cristiano', 1, NULL),
+('Blue', 'Kupe', 2, 1),
+('Indiana', 'Jones', 3, 2),
+('Cantona', 'Triston', 4, NULL),
+('Ben', 'Wyatt', 5, NULL),
+('Marko', 'Polo', 6, NULL),
---
+++ server.js
@@ -0,0 +1,7 @@
+//required dependancies
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const CTable = require("console.table");
+const db = require('./db/connection');
+
+
