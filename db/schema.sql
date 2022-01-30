a26a558ad13bf6443efef366ed867f0bb626eacb
benjamin amakye <bamakye20@gmail.com>
2022-01-29 23:33:32 -0500

    second commit

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
+    name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
@@ -13,18 +13,16 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL (5,2),
+    salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY (id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  roles_id INTEGER,
-  manager_id INTEGER,
-  FOREIGN KEY (roles_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES roles(id)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  job_id INT,
+  manager_id INT
 );
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES 
+('Corporate and planning'),
+('IT'),
+('Inside Sales'),
+('budget and financing'),
+('HR')
+
+INSERT INTO job (title, salary, department_id)
+VALUES 
+('Director', 75000, 1),
+('Associate', 18000, 1),
+('Head Auditor', 100000, 2),
+('Accountant', 80000, 2),
+('Engineer', 55000, 3),
+('Sales rep', 0, 1);
